3f64ef2cfd61f994baa664b805aa83146caf73d7
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-03-08 04:33:18 +0700

    Initial

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -6,9 +6,14 @@ use Filament\Pages;
 use Filament\Panel;
 use Filament\Widgets;
 use Filament\PanelProvider;
+use Filament\Navigation\MenuItem;
 use Filament\Support\Colors\Color;
+use Illuminate\Support\HtmlString;
+use Filament\Support\Enums\MaxWidth;
+use App\Filament\Resources\UserResource;
 use Filament\Http\Middleware\Authenticate;
 use Illuminate\Session\Middleware\StartSession;
+use Devonab\FilamentEasyFooter\EasyFooterPlugin;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Filament\Http\Middleware\AuthenticateSession;
 use Illuminate\Routing\Middleware\SubstituteBindings;
@@ -18,6 +23,7 @@ use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 
+
 class AdminPanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
@@ -25,6 +31,12 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->spa()
+            // ->brandLogo(fn() => view('logo'))
+            // ->brandLogoHeight('1.25rem')
+            ->maxContentWidth(MaxWidth::Full)
+            ->unsavedChangesAlerts()
+            ->databaseNotifications()
+            ->topNavigation()
             ->id('admin')
             ->path('admin')
             ->login()
@@ -34,6 +46,12 @@ class AdminPanelProvider extends PanelProvider
             ->profile()
             ->colors([
                 'primary' => Color::Cyan,
+            ])
+            ->userMenuItems([
+                MenuItem::make()
+                    ->label('Pengguna')
+                    ->url(fn(): string => UserResource::getUrl())
+                    ->icon('heroicon-o-identification'),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
@@ -58,6 +76,27 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                EasyFooterPlugin::make()
+                    // ->footerEnabled() // true by default,
+                    ->withFooterPosition('footer')
+                    ->withGithub(showLogo: true, showUrl: true)
+                    ->withLoadTime('Halaman ini dimuat dalam')
+                    ->withSentence(
+                        new HtmlString(
+                            '<b>Madrasah Tsanawiyah Negeri 1 Pandeglang</b> | Dibuat dan dikembangkan dengan ❤ oleh <a href="https://instagram.com/zulfikriyahya_"><b>Yahya Zulfikri</b></a>'
+                        )
+                    )
+                    ->withLinks([
+                        ['title' => 'Official Website', 'url' => 'https://www.mtsn1pandeglang.sch.id/'],
+                        ['title' => 'Kebijakan Privasi', 'url' => 'https://www.mtsn1pandeglang.sch.id/kebijakan-privasi/'],
+                        ['title' => 'Tentang Kami', 'url' => 'https://www.mtsn1pandeglang.sch.id/about/'],
+                        // ['title' => 'Kontak', 'url' => 'https://wa.me/6289612050291/?text=Hallo,%20PTSP%20MTs%20Negeri%201%20Pandeglang.'],
+                    ])
+                    ->withBorder()
+                // ->hiddenFromPagesEnabled()
+                // ->hiddenFromPages(['admin/login', 'admin/forgot-password', 'admin/register', 'admin/email/verify']),
             ]);
     }
 }
---
+++ config/filament-easy-footer.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'app_name' => env('APP_NAME', 'Filament Footer'),
+    'github' => [
+        'repository' => env('GITHUB_REPOSITORY', 'https://github.com/zulfikriyahya/admisi.git'),
+        'token' => env('GITHUB_TOKEN', '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'),
+        'cache_ttl' => env('GITHUB_CACHE_TTL', 3600),
+    ],
+];
