802d4505d1c7295681f4c5a1203a76348591bdf8
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-03-08 22:57:42 +0700

    Initial

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
+    // App\Providers\FortifyServiceProvider::class,
+    // App\Providers\JetstreamServiceProvider::class,
 ];
