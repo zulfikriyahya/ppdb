5f13f78b7088b9a1ed987a05575f1583c7ca9eae
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-03-17 05:50:21 +0700

    Refactor

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -10,13 +10,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('username')->unique();
-            $table->string('email')->unique();
+            $table->string('name', 50);
+            $table->string('username', 10)->unique();
+            $table->string('email', 50)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->string('avatar', 2048)->nullable();
             $table->timestamps();
             $table->softDeletes();
         });
