8685af14f55e101ff2f86f617632a79ea6af1299
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-03-24 05:30:40 +0700

    Refactor

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,27 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use Database\Seeders\RoleSeeder;
-use Database\Seeders\UserSeeder;
-use Database\Seeders\KelasSeeder;
-use Database\Seeders\KetuaSeeder;
-use Database\Seeders\NegaraSeeder;
-use Database\Seeders\AnggotaSeeder;
-use Database\Seeders\JurusanSeeder;
-use Database\Seeders\SekolahSeeder;
-use Database\Seeders\PimpinanSeeder;
-use Database\Seeders\PrestasiSeeder;
-use Database\Seeders\ProvinsiSeeder;
-use Database\Seeders\BendaharaSeeder;
-use Database\Seeders\KabupatenSeeder;
-use Database\Seeders\KecamatanSeeder;
-use Database\Seeders\KelurahanSeeder;
-use Database\Seeders\SekretarisSeeder;
-use Database\Seeders\SekolahAsalSeeder;
-use Database\Seeders\MataPelajaranSeeder;
-use Database\Seeders\EkstrakurikulerSeeder;
-use Database\Seeders\JalurPendaftaranSeeder;
-use Database\Seeders\TahunPendaftaranSeeder;
 
 class DatabaseSeeder extends Seeder
 {
