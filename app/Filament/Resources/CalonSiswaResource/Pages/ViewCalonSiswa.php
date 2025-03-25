68cc5a7acacae233ae375033994c9d396494c521
Yahya Zulfikri <zulfikriyahya18@gmail.com>
2025-03-26 05:41:24 +0700

    Refactor

--- app/Filament/Resources/CalonSiswaResource/Pages/ViewCalonSiswa.php
+++ app/Filament/Resources/CalonSiswaResource/Pages/ViewCalonSiswa.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\CalonSiswaResource\Pages;
 
 use Carbon\Carbon;
+use App\Models\CalonSiswa;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Filament\Resources\Pages\ViewRecord;
@@ -170,11 +171,70 @@ class ViewCalonSiswa extends ViewRecord
                     $tahunPendaftaran = DB::table('tahun_pendaftarans')
                         ->where('status', 'Aktif')
                         ->first();
-                    $currentDate = Carbon::now();
-                    $startDate = Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_penerbitan_kartu_tes_mulai));
-                    $endDate = Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_penerbitan_kartu_tes_selesai));
 
-                    if ($currentDate->lt($startDate) || $currentDate->gt($endDate)) {
+                    // Cek Juga Apakah Status Pendaftarannya Adalah Diterima || Diterima di kelas Unggulan || Diterima di kelas Reguler
+                    $calonSiswa = CalonSiswa::where('nisn', Auth::user()->username)->first();
+                    // dd($calonSiswa);
+
+
+                    $currentDate = Carbon::now();
+
+                    $mulaiPengumumanPrestasi = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_prestasi_mulai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_prestasi_mulai))
+                        : null;
+
+                    $akhirPengumumanPrestasi = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_prestasi_selesai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_prestasi_selesai))
+                        : null;
+
+                    $mulaiPengumumanReguler = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_reguler_mulai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_reguler_mulai))
+                        : null;
+
+                    $akhirPengumumanReguler = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_reguler_selesai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_reguler_selesai))
+                        : null;
+
+                    $mulaiPengumumanAfirmasi = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_afirmasi_mulai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_afirmasi_mulai))
+                        : null;
+
+                    $akhirPengumumanAfirmasi = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_afirmasi_selesai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_afirmasi_selesai))
+                        : null;
+
+                    $mulaiPengumumanZonasi = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_zonasi_mulai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_zonasi_mulai))
+                        : null;
+
+                    $akhirPengumumanZonasi = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_zonasi_selesai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_zonasi_selesai))
+                        : null;
+
+                    $mulaiPengumumanMutasi = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_mutasi_mulai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_mutasi_mulai))
+                        : null;
+
+                    $akhirPengumumanMutasi = !empty($tahunPendaftaran->tanggal_pengumuman_jalur_mutasi_selesai)
+                        ? Carbon::createFromFormat('Y-m-d H:i:s', trim($tahunPendaftaran->tanggal_pengumuman_jalur_mutasi_selesai))
+                        : null;
+
+                    if (
+                        ($mulaiPengumumanPrestasi === null || $akhirPengumumanPrestasi === null ||
+                            $currentDate->lt($mulaiPengumumanPrestasi) || $currentDate->gt($akhirPengumumanPrestasi) || ($calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Reguler' && $calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Unggulan'))
+                        ||
+                        ($mulaiPengumumanReguler === null || $akhirPengumumanReguler === null ||
+                            $currentDate->lt($mulaiPengumumanReguler) || $currentDate->gt($akhirPengumumanReguler) || ($calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Reguler' && $calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Unggulan'))
+                        ||
+                        ($mulaiPengumumanAfirmasi === null || $akhirPengumumanAfirmasi === null ||
+                            $currentDate->lt($mulaiPengumumanAfirmasi) || $currentDate->gt($akhirPengumumanAfirmasi) || ($calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Reguler' && $calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Unggulan'))
+                        ||
+                        ($mulaiPengumumanZonasi === null || $akhirPengumumanZonasi === null ||
+                            $currentDate->lt($mulaiPengumumanZonasi) || $currentDate->gt($akhirPengumumanZonasi) || ($calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Reguler' && $calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Unggulan'))
+                        ||
+                        ($mulaiPengumumanMutasi === null || $akhirPengumumanMutasi === null ||
+                            $currentDate->lt($mulaiPengumumanMutasi) || $currentDate->gt($akhirPengumumanMutasi) || ($calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Reguler' && $calonSiswa->status_pendaftaran !== 'Diterima Di Kelas Unggulan'))
+                    ) {
                         return false;
                     }
                     return true;
